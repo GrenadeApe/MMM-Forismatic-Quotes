41be059517d8ed33dd478febf37c0bc7aa03c70b
Michael <admin@randal.io>
2017-06-27 21:52:39 -0400

    CBD Rework upload

--- MMM-Forismatic-Quotes.js
+++ MMM-Forismatic-Quotes.js
@@ -11,96 +11,51 @@ Module.register("MMM-Forismatic-Quotes", {
 	defaults: {
 		updateInterval: 60000,
 		retryDelay: 5000,
-		animationSpeed: 5000,
+		animationSpeed: 5,
 		lang: "en",
-		key: 123456,
-		apiURL: "http://api.forismatic.com/api/1.0/"
+		key: "123456"
+		
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
 
 	start: function() {
-		var self = this;
-		var dataRequest = null;
-		var dataNotification = null;
-
-		//Flag for check if module is loaded
-		this.loaded = false;
-
-		// Schedule update timer.
-		this.getData();
-		setInterval(function() {
-			self.updateDom();
-		}, this.config.updateInterval);
-	},
-
-	/*
-	 * getData
-	 * function example return data and show it in the module wrapper
-	 * get a URL request
-	 *
-	 */
-	getData: function() {
-		var self = this;
 		
-		var urlApi = this.config.apiURL + "?method=getQuote&format=json&lang=" + this.config.lang + "&key=" + this.config.key;
+        this.url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=" + this.config.lang + "&key=" + this.config.key;
+        this.scheduleUpdate();
+    },
 
-		var retry = true;
-		
-		var str_key = self.config.key.toString();
-		
-		if (this.config.key.length() > 6) {
-			Log.error(self.name, "Max key length is 6.");
-		}
-		
-		if (this.config.lang != "en" || "ru") {
-			Log.error(self.name, "Please enter a supported language into the config.");
-		}
+		getQuote: function() {
+        this.sendSocketNotification('GET_QUOTE', this.url);
+    },
 
-		var dataRequest = new XMLHttpRequest();
-		dataRequest.open("GET", urlApi, true);
-		dataRequest.onreadystatechange = function() {
-			console.log(this.readyState);
-			if (this.readyState === 4) {
-				console.log(this.status);
-				if (this.status === 200) {
-					self.processData(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
-				} else {
-					Log.error(self.name, "Could not load data.");
-				}
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		dataRequest.send();
-	},
+      socketNotificationReceived: function(notification, payload) {
+        if (notification === "QUOTE_RESULT") {
+            this.processQuote(payload);
+            if (this.rotateInterval == null) {
+                this.scheduleCarousel();
+            }
+        }
+        this.updateDom(this.config.initialLoadDelay);
+    },
 
+	scheduleUpdate: function() {
+        setInterval(() => {
+            this.getQuote();
+        }, this.config.updateInterval);
+        this.getQuote(this.config.initialLoadDelay);
+    },
 
-	/* scheduleUpdate()
-	 * Schedule next update.
-	 *
-	 * argument delay number - Milliseconds before next update.
-	 *  If empty, this.config.updateInterval is used.
-	 */
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		nextLoad = nextLoad ;
-		var self = this;
-		setTimeout(function() {
-			self.getData();
-		}, nextLoad);
-	},
+     processQuote: function(data) {
+        this.today = data.Today;
+        this.quote = data;
+		console.log(this.quote);
+        this.loaded = true;
+    },
+
 
 	getDom: function() {
-		var self = this;
+
 
 		// create element wrapper for show into the module
 		var wrapper = document.createElement("div");
@@ -111,42 +66,30 @@ Module.register("MMM-Forismatic-Quotes", {
 			return wrapper;
 		}
 		// If this.dataRequest is not empty
-		if (this.dataRequest) {
-			var wrapperDataRequest = document.createElement("div");
+		 var quote = this.quote;
+		 
+		 
+			var wrapperData = document.createElement("div");
 			
-			var wrapperDataRequestQuote = document.createElement("div");
-			wrapperDataRequestQuote.innerHTML = this.quoteText;
-			wrapperDataRequestQuote.className = "bold xlarge bright align-left";
+			var Quote = document.createElement("div");
+			Quote.innerHTML = quote.quoteText;
+			Quote.className = "bold xlarge bright align-left";
 			
-			var wrapperDataRequestAuthor = document.createElement("div");
-			wrapperDataRequestAuthor.innerHTML = this.quoteAuthor;
-			wrapperDataRequestAuthor.className = "regular medium normal align-right";
+			var QuoteAuthor = document.createElement("div");
+			QuoteAuthor.innerHTML = quote.quoteAuthor;
+			QuoteAuthor.className = "regular medium normal align-right";
 			
-			var wrapperDataRequestLink = document.createElement("div");
-			wrapperDataRequestLink.innerHTML = this.quoteLink;
-			wrapperDataRequestLink.className = "thin xsmall dimmed align-right";
+			var QuoteLink = document.createElement("div");
+			QuoteLink.innerHTML = quote.quoteLink;
+			QuoteLink.className = "thin xsmall dimmed align-right";
 			
-			wrapperDataRequest.appendChild(wrapperDataRequestQuote);
-			wrapperDataRequest.appendChild(wrapperDataRequestAuthor);
-			wrapperDataRequest.appendChild(wrapperDataRequestLink);
+			wrapperData.appendChild(Quote);
+			wrapperData.appendChild(QuoteAuthor);
+			wrapperData.appendChild(QuoteLink);
 			
-			wrapper.appendChild(wrapperDataRequest);
+			wrapper.appendChild(wrapperData);
 		}
 		return wrapper;
-	},
-
-	processData: function (data) {
-		
-		if (!data || !data.quoteText) {
-			// Did not receive usable new data.
-			return;
-		}
-		
-		this.quoteText = data.quoteText;
-		this.quoteAuthor = data.quoteAuthor;
-		this.quoteLink = data.quoteLink;
-		
-		this.loaded = true;
-		this.updateDom(this.config.animationSpeed);
-	},
+	}
+	
 });
--- node_helper.js
+++ node_helper.js
@@ -1,35 +1,36 @@
 /* Magic Mirror
-    * Module: MMM-Forismatic-Quotes
-    *
-    * Adapted from MMM-MyWeather
-    * By Cowboysdude
-    * MIT Licensed.
-    */
+ * Module: MMM-Forismatic-Quotes
+ *
+ * By Mykle1
+ *
+ */
 const NodeHelper = require('node_helper');
-var request = require("request");
+const request = require('request');
+
+
 
 module.exports = NodeHelper.create({
 
     start: function() {
-       
-        console.log("Starting module: " + this.name);
+        console.log("Starting node_helper for: " + this.name);
     },
 
-    getWeather: function(url) {
+    getQuote: function(url) {
         request({
             url: url,
             method: 'GET'
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
                 var result = JSON.parse(body);
-                this.sendSocketNotification('WEATHER_RESULT', result);
+				console.log(result);
+                    this.sendSocketNotification('QUOTE_RESULT', result);
             }
         });
     },
 
     socketNotificationReceived: function(notification, payload) {
-        if (notification === 'GET_WEATHER') {
-            this.getWeather(payload);
+        if (notification === 'GET_QUOTE') {
+            this.getQuote(payload);
         }
     }
 });
